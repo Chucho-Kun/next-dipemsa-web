f1e25c1ae8da985f844c3add088808e43f186ed2
Jesus Peralta <jesus_web_master@hotmail.com>
2026-05-30 15:57:40 -0600

    Searching Bar ready

--- src/shared/components/GroupCard.tsx
+++ src/shared/components/GroupCard.tsx
@@ -45,7 +45,7 @@ export default function GroupCard({ group }: { group: GroupedProduct }) {
 
       {/* Contenido */}
       <div className="flex-1 flex flex-col p-5">
-        <p className="text-xs font-medium text-gray-500">{ group.variants[0].marca }</p>
+        <p className="text-xs font-medium text-gray-500 uppercase">{ group.variants[0].marca }</p>
         
         <h3 className="font-semibold text-lg leading-tight mt-1 mb-4 line-clamp-2">
           {mainName}
