77027a47a182be5150b6d9f98789c0c668d22dad
Jesus Peralta <desarrolladorwebodm@gmail.com>
2026-05-29 18:49:14 -0600

    categories result displayed

--- app/(public)/marca/[slug]/page.tsx
+++ app/(public)/marca/[slug]/page.tsx
@@ -1,12 +1,25 @@
 import TrademarckResults from "@/src/shared/components/TrademarckResults";
+import { slugToMarca } from "@/src/shared/db/queries";
+import { Metadata } from "next";
+
+// Metadata dinámica
+export async function generateMetadata(props: PageProps<'/categoria/[slug]'>): Promise<Metadata> {
+  const { slug } = await props.params;
+  const marcaNombre = slugToMarca(slug);
+
+  return {
+    title: `Dipemsa | ${marcaNombre}`,
+    description: `Explora productos de la marca ${marcaNombre.toLowerCase()}`,
+    openGraph: {
+      title: `${marcaNombre}`,
+    }
+  };
+}
 
 export default async function MarcaResultPage(props: PageProps<'/marca/[slug]'>) {
 
   const { slug } = await props.params
-
-  console.log(slug);
   
-
   return (
         <TrademarckResults slug={ slug } />
   )
