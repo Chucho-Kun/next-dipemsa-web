9243fe3b9747df9e4bceebc3b3e1bfb6cb8f0461
Jesus Peralta <desarrolladorwebodm@gmail.com>
2026-06-03 11:54:00 -0600

    Google Analitycs added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Jost } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast"
+import Script from "next/script";
 
 
 const jost = Jost({
@@ -28,7 +29,27 @@ export default function RootLayout({
       lang="es"
       className={`${jost.variable} h-full bg-white antialiased`}
     >
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            <!-- Google Tag Manager -->
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M4B5NXB');</script>
+            <!-- End Google Tag Manager -->
+          `}
+        </Script>
+      </head>
       <body className="min-h-full flex flex-col font-jost">
+
+          {/* Google Tag Manager (noscript) */}
+          <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M4B5NXB" height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe>
+          </noscript>
+
         {children}
         <Toaster
           position="top-center"
