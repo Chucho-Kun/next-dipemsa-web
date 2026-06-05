5e713cfc9365a77f6c5725febb0ce7859bf2d532
Jesus Peralta <desarrolladorwebodm@gmail.com>
2026-06-05 13:59:26 -0600

    new 404 page with redirection

---
+++ app/not-found.tsx
@@ -0,0 +1,22 @@
+// app/not-found.tsx
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function NotFound() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/'); // Redirige al home
+  }, [router]);
+
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-7xl font-bold">404</h1>
+        <p>Redirigiendo al inicio...</p>
+      </div>
+    </div>
+  );
+}
