51279924f99191e06f1ca89f1335fd9eed9e7355
Jesus Peralta <desarrolladorwebodm@gmail.com>
2026-06-22 16:06:00 -0600

    setting new port to mail

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -17,8 +17,8 @@ const getTransporter = () => {
 
   transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: Number(process.env.EMAIL_PORT) || 587,
-    secure: false,
+    port: 465, //Number(process.env.EMAIL_PORT) || 587,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
@@ -27,7 +27,6 @@ const getTransporter = () => {
       rejectUnauthorized: false,
     },
     connectionTimeout: 15000,
-    greetingTimeout: 15000,
     socketTimeout: 30000,        
   });
 
