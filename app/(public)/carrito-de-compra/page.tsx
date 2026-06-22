16dec04176725cef989b9f8e2c2109d353e9df61
Jesus Peralta <desarrolladorwebodm@gmail.com>
2026-06-22 10:11:13 -0600

    fixed deployment

--- app/(public)/carrito-de-compra/page.tsx
+++ app/(public)/carrito-de-compra/page.tsx
@@ -1,4 +1,5 @@
 import ResumenCompraPage from "@/src/shared/components/cart/ResumenCompra";
+import { Suspense } from "react";
 
 export const metadata = {
   title: 'Dipemsa | Carrito de Compra',
@@ -22,6 +23,8 @@ export const metadata = {
 
 export default function CompraPage() {
   return (
-    <ResumenCompraPage />
+    <Suspense fallback={ <div>Cargando...</div> }>
+      <ResumenCompraPage />
+    </Suspense>
   )
 }
