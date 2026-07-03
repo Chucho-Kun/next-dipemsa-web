cb9d99cb0240b50e7c5bee3e4d0214c1176d73e6
Jesus Peralta <desarrolladorwebodm@gmail.com>
2026-07-03 15:50:53 -0600

    new SEO feature

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,6 +1,6 @@
 import { db } from '@/src/shared/db';
 import { productos } from '@/src/shared/db/schema/productList';
-import { ilike, desc, or } from 'drizzle-orm';
+import { ilike, desc, or, sql } from 'drizzle-orm';
 import { NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
@@ -12,6 +12,7 @@ export async function GET(request: NextRequest) {
 
   const results = await db.select({
     id: productos.id,
+    clave: productos.clave,
     descripcion: productos.descripcion,
     precioant: productos.precioant,
     precio: productos.precio,
@@ -20,13 +21,21 @@ export async function GET(request: NextRequest) {
   .from(productos)
   .where(
     or(
-      ilike(productos.marca, `%${search}%`),
+      ilike(productos.clave, `%${search}%`),
       ilike(productos.descripcion, `%${search}%`),
-      ilike(productos.clave, `%${search}%`)
+      ilike(productos.marca, `%${search}%`)
     )
   )
-  .orderBy(desc(productos.destacado))
-  .limit(10);
+  .orderBy(
+    sql`CASE 
+          WHEN clave ILIKE ${'%' + search + '%'} THEN 1 
+          WHEN descripcion ILIKE ${'%' + search + '%'} THEN 2 
+          WHEN marca ILIKE ${'%' + search + '%'} THEN 3 
+          ELSE 4 
+        END`,
+    desc(productos.destacado)
+  )
+  .limit(15);
 
   return Response.json(results);
 }
